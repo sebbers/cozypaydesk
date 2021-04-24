6ff3a26a753831fed0d246aa57e6dfd005670fa3
sebbers <sebb.roeder@gmail.com>
2021-04-24 14:08:01 -0700

    landing page

--- client/src/components/pages/LandingPage.js
+++ client/src/components/pages/LandingPage.js
@@ -4,10 +4,7 @@ const LandingPage = () => {
   return (
     <>
     <div style={{textAlign: "center"}}>
-       <span style={{ fontSize: '2rem' }}>Happy Coding!<span>😊</span></span>
-       <div>🔥 Thanks for using the Boiler Plate 🔥 </div> 
-       <div>Check the details on <a href="https://www.github.com/PrakadAlpha/mern-boiler-plate">Github</a></div>
-       <div>Checkout my <a href="https://www.prakadalpha.me">Blog</a></div> 
+       Welcome
     </div>
     </>
   )
